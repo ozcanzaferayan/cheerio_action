3e9d786a8bf65db763242d7a4a619080dc92d698
Zafer AYAN <ozcanzaferayan@gmail.com>
2023-01-27 02:30:39 +0300

    Add

--- src/utils/dateUtil.test.ts
+++ src/utils/dateUtil.test.ts
@@ -1,4 +1,4 @@
-import { isSentToday } from "../utils/dateUtil";
+import { isSentToday, getDayDiff, isObjectEmpty } from "../utils/dateUtil";
 import { hash } from "../utils/hashUtil";
 
 describe("dateUtil", () => {
@@ -30,6 +30,38 @@ describe("dateUtil", () => {
       };
       const isSent = isSentToday(dbUrlRecord, "www.example.com");
       expect(isSent).toBe(false);
+    });
+
+    it("should return false when isSent is false", () => {
+      const exampleHash = hash("www.example.com");
+      const dbUrlRecord = {
+        [exampleHash]: {
+          isSent: false,
+          date: "2023-01-25T22:04:08.655Z",
+        },
+      };
+      const isSent = isSentToday(dbUrlRecord, "www.example.com");
+      expect(isSent).toBe(false);
+    });
+
+    it("should return false when dbUrlRecord is empty", () => {
+      const dbUrlRecord = {};
+      const isSent = isSentToday(dbUrlRecord, "www.example.com");
+      expect(isSent).toBe(false);
+    });
+    it("should return true when object is empty", () => {
+      const obj = isObjectEmpty({});
+      expect(obj).toBe(true);
+    });
+  });
+
+  describe("getDayDiff", () => {
+    it("should return 3 when given 3 days before", () => {
+      const dayDiff = getDayDiff(
+        new Date().getTime(),
+        new Date("2023-01-24T20:00:00.000Z").getTime()
+      );
+      expect(dayDiff).toBe(3);
     });
   });
 });
--- src/utils/dateUtil.ts
+++ src/utils/dateUtil.ts
@@ -5,10 +5,19 @@ export function isSentToday(
   dbUrlRecord: Record<string, UrlSent>,
   url: string
 ): boolean {
+  // if (isObjectEmpty(dbUrlRecord)) return true;
   const obj = dbUrlRecord[hash(url)];
   if (!obj?.isSent) return false;
   const sentDate = new Date(obj.date).getTime();
   const today = new Date().getTime();
-  const dayDiff = Math.floor((today - sentDate) / (1000 * 60 * 60 * 24));
+  const dayDiff = getDayDiff(today, sentDate);
   return dayDiff === 0;
+}
+
+export function getDayDiff(today: number, sentDate: number): number {
+  return Math.floor((today - sentDate) / (1000 * 60 * 60 * 24));
+}
+
+export function isObjectEmpty(obj: object) {
+  return Object.keys(obj).length === 0;
 }
